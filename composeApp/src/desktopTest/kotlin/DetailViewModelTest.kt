39ab52de825189a881e0fd05ab4b7bae40e00d73
Federico Urquiza <federicourquizaranieri@gmail.com>
2025-06-16 16:43:20 -0300

    Creacion de test de detailsViewModel
    Implementacion completa (con test que no recupera aun los 2 estados)

--- composeApp/src/desktopTest/kotlin/DetailViewModelTest.kt
+++ composeApp/src/desktopTest/kotlin/DetailViewModelTest.kt
@@ -1,2 +1,104 @@
+import edu.dyds.movies.domain.entity.Movie
+import edu.dyds.movies.domain.usecase.GetMovieDetailsUseCase
+import edu.dyds.movies.domain.usecase.MovieDetailsUseCase
+import edu.dyds.movies.presentation.detail.DetailViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
 class DetailViewModelTest {
+
+    val testDispatcher = UnconfinedTestDispatcher()
+    val testScope = CoroutineScope(testDispatcher)
+    private val getMovieDetailsUseCase = object : MovieDetailsUseCase {
+        override suspend fun invoke(id: Int): Movie? {
+            return if (id==1)
+                Movie(
+                    id,
+                    "Movie $id",
+                    "the movie $id overview",
+                    "21/10/2023",
+                    "poster url",
+                    "backdrop url",
+                    "Original Movie $id",
+                    "en",
+                    10.0,
+                    8.0
+                )
+            else
+                null
+        }
+    }
+    private lateinit var detailViewModel: DetailViewModel
+
+    @BeforeEach
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        detailViewModel = DetailViewModel(getMovieDetailsUseCase)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `get movie should emit loading and data states`() = runTest {
+        // Arrange
+        val events: ArrayList<DetailViewModel.MovieDetailUiState> = arrayListOf()
+        testScope.launch {
+            detailViewModel.movieDetailStateFlow.collect { state ->
+                events.add(state)
+            }
+        }
+
+        // Act
+        detailViewModel.getMovieDetails(1)
+
+        // Assert
+        assertEquals(DetailViewModel.MovieDetailUiState(true, null),events[0])
+        assertEquals(
+            DetailViewModel.MovieDetailUiState(
+                false,
+                Movie(
+                    1,
+                    "Movie 1",
+                    "the movie 1 overview",
+                    "21/10/2023",
+                    "poster url",
+                    "backdrop url",
+                    "Original Movie 1",
+                    "en",
+                    10.0,
+                    8.0
+                )
+            ),
+            events[1]
+        )
+    }
+    @Test
+    fun `get movie emits loading and null`() =runTest{
+        // Arrange
+        val events: ArrayList<DetailViewModel.MovieDetailUiState> = arrayListOf()
+        testScope.launch {
+            detailViewModel.movieDetailStateFlow.collect { state ->
+                events.add(state)
+            }
+        }
+        // Act
+        detailViewModel.getMovieDetails(3)
+        // Assert
+        assertEquals(DetailViewModel.MovieDetailUiState(true, null),events[0])
+        assertEquals(DetailViewModel.MovieDetailUiState(false, null),events[1])
+    }
 }
